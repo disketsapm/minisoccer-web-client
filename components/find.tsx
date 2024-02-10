69b663a452af70fb651139e528422c949bb1ae7e
deprasny <deprasny@gmail.com>
2024-02-10 18:15:52 +0700

    Update component sizes and imports

--- components/find.tsx
+++ components/find.tsx
@@ -11,7 +11,7 @@ export default function Find() {
       <div className=" h-full w-1/2 rounded-3xl overflow-hidden">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.938525379262!2d106.76214517453191!3d-6.271814661395947!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f0526938c0bf%3A0x945ab245061f8415!2sJl.%20RC.%20Veteran%20Raya%20No.1%2C%20RT.9%2FRW.3%2C%20Bintaro%2C%20Kec.%20Pesanggrahan%2C%20Kota%20Jakarta%20Selatan%2C%20Daerah%20Khusus%20Ibukota%20Jakarta%2012330!5e0!3m2!1sid!2sid!4v1707502591058!5m2!1sid!2sid"
-          width="630"
+          width="730"
           height="300"
           style={{ border: 0 }}
           loading="lazy"
--- components/hero.tsx
+++ components/hero.tsx
@@ -1,43 +1,37 @@
-"use client";
+'use client';
 
-import Image from "next/image";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious
-} from "./ui/carousel";
-import { Card, CardContent } from "./ui/card";
-import Autoplay from "embla-carousel-autoplay";
+import Image from 'next/image';
+import { Carousel, CarouselContent, CarouselItem } from './ui/carousel';
+import { Card } from './ui/card';
+import Autoplay from 'embla-carousel-autoplay';
 
 export default function Hero() {
-  const imagesLink = ["/images/banner.png", "/images/ronaldo.png"];
+  const imagesLink = ['/images/banner.png', '/images/ronaldo.png'];
   return (
     <section className="">
       <Carousel
         plugins={[
           Autoplay({
-            delay: 3000
-          })
+            delay: 3000,
+          }),
         ]}
         opts={{
-          align: "start",
-          loop: true
+          align: 'start',
+          loop: true,
         }}
       >
         <CarouselContent>
           {imagesLink.map((_, index) => (
             <CarouselItem key={index}>
-              <Card className="bg-hero-pattern bg-cover bg-center flex items-center justify-center rounded-3xl overflow-hidden">
+              <div className=" flex items-center justify-center ">
                 <Image
                   src={imagesLink[index]}
                   alt="Hero"
-                  layout="responsive"
-                  width={1000}
-                  height={100}
+                  width={1300}
+                  height={300}
+                  className="object-cover rounded-3xl bg-cover "
                 />
-              </Card>
+              </div>
             </CarouselItem>
           ))}
         </CarouselContent>
