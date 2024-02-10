f1d708d24a811f888bdae7102ace5699b3d79ef3
Depras Nur Yadi <deprasny@gmail.com>
2024-02-10 11:05:50 +0700

    Update label in header component

--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,7 @@ import { Button } from "./ui/button";
 export default function Header() {
   const links = [
     { href: "/", label: "Booking" },
-    { href: "/about", label: "Tentang Kami" },
+    { href: "/about", label: "Tentang Kamisssssssss" },
     { href: "/about", label: "Kerja Sama" },
     { href: "/about", label: "Hubungi Kami" }
   ];
