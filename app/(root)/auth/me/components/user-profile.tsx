093de28dc9a7a4522500e8b14ce9ba73a502e43a
mrayandika15 <mrayandika15@gmail.com>
2024-04-17 14:39:01 +0700

    Update user profile component with skeleton loading and user data retrieval

--- app/(root)/auth/me/components/user-profile.tsx
+++ app/(root)/auth/me/components/user-profile.tsx
@@ -3,21 +3,52 @@
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
 import { useLogout } from "@/hooks/auth/useLogout";
 import { getTokenFromLocalStorage } from "@/lib/utils";
+import { AuthService } from "@/services/auth.service";
+import { useQuery } from "@tanstack/react-query";
 
 import React from "react";
 
 const UserProfile = () => {
   const { mutateAsync, isPending } = useLogout();
 
-  return (
-    <Card className="border-2 p-4 border-black rounded-xl h-[fit-content] flex flex-col gap-2 items-center">
-      <Avatar className="">
-        <AvatarFallback>MR</AvatarFallback>
-      </Avatar>
+  const authService = new AuthService();
 
-      <p className="font-semibold text-lg text-center">Muhammad Rayandika</p>
+  const { data, isLoading } = useQuery({
+    queryKey: ["me"],
+    queryFn: async () => authService.getUserDetail(),
+  });
+
+  const getFirstLetterAndLastName = (fullName: string) => {
+    const name = fullName?.split(" ");
+    return `${name?.[0]?.charAt(0)}${name?.[name?.length - 1].charAt(0)}`;
+  };
+
+  return (
+    <Card className="border-2 p-4 md:w-[250px] w-full  border-black rounded-xl h-[fit-content] flex flex-col gap-2 items-center">
+      {isLoading ? (
+        <Skeleton className="w-[120px] h-[120px] rounded-full" />
+      ) : (
+        <Avatar className="w-[120px] h-[120px] font-semibold text-xl ">
+          <AvatarFallback>
+            {getFirstLetterAndLastName(data?.fullName)}
+          </AvatarFallback>
+        </Avatar>
+      )}
+
+      {isLoading ? (
+        <Skeleton className="w-[120px] h-[30px]" />
+      ) : (
+        <p className="font-semibold text-lg text-center">{data?.fullName}</p>
+      )}
+
+      {isLoading ? (
+        <Skeleton className="w-[120px] h-[30px]" />
+      ) : (
+        <p className="text-sm text-center">{data?.email}</p>
+      )}
 
       <Button
         variant="accent-2"
