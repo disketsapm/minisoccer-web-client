ff405d0f7d96228e607c6876c41407890661da2c
mrayandika15 <mrayandika15@gmail.com>
2024-04-17 15:05:26 +0700

    Update user profile component with AvatarImage and usePathname hook

--- app/(root)/auth/me/components/user-profile.tsx
+++ app/(root)/auth/me/components/user-profile.tsx
@@ -7,11 +7,14 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { useLogout } from "@/hooks/auth/useLogout";
 import { getTokenFromLocalStorage } from "@/lib/utils";
 import { AuthService } from "@/services/auth.service";
+import { AvatarImage } from "@radix-ui/react-avatar";
 import { useQuery } from "@tanstack/react-query";
+import { usePathname } from "next/navigation";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const UserProfile = () => {
+  const pathname = usePathname();
   const { mutateAsync, isPending } = useLogout();
 
   const authService = new AuthService();
@@ -26,12 +29,28 @@ const UserProfile = () => {
     return `${name?.[0]?.charAt(0)}${name?.[name?.length - 1].charAt(0)}`;
   };
 
+  const [dataUser, setDataUser] = useState({} as any);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      try {
+        const user = JSON.parse(localStorage.getItem("user") as string);
+        if (user) {
+          setDataUser(user);
+        }
+      } catch (error) {
+        console.error("Failed to parse user data from localStorage:", error);
+      }
+    }
+  }, [pathname]);
+
   return (
     <Card className="border-2 p-4 md:w-[250px] w-full  border-black rounded-xl h-[fit-content] flex flex-col gap-2 items-center">
       {isLoading ? (
         <Skeleton className="w-[120px] h-[120px] rounded-full" />
       ) : (
-        <Avatar className="w-[120px] h-[120px] font-semibold text-xl ">
+        <Avatar className="w-[120px] h-[120px] font-semibold text-xl">
+          <AvatarImage src={dataUser?.photo} />
           <AvatarFallback>
             {getFirstLetterAndLastName(data?.fullName)}
           </AvatarFallback>
